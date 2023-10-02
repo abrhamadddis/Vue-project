03e29821ed1489321ce8c710d8cb59a60441217b
abrhamadddis <abrhamaddis32@gmail.com>
2023-10-02 22:44:14 +0300

    seting up basic env for the project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <h1 class="text-3xl font-bold underline" >test</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
